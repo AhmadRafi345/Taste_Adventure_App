f390e92959ab67436670f71a8f3a671eaa64eec5
Ahmad Rafi <rafimars345@gmail.com>
2024-11-10 00:16:38 +0700

    first commit

---
+++ lib/core/constants/image_constants.dart
@@ -0,0 +1,5 @@
+class ImageConstants {
+  static const String ingredients = 'assets/images/download.jpeg';
+  static const String waffle = 'assets/images/waffle2.jpeg';
+  static const String pancakes = 'assets/images/Pancakes2.jpeg';
+}
